54dcf16fcb790c4bf4ec312e8e87f03935904f54
Thilak <thilaksaithipparapu6300@gmail.com>
2023-11-03 22:27:13 +0530

    Added unique constraint and started cityrepository

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require(`./config/serverConfig`);
+const CityRepository = require('./repository/city-repository');
 
 const setupAndStartServer = async ()=>{
-    //create the express object
+    //create the express object 
     const app = express();
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
    
-    app.listen(PORT,()=>{
+    app.listen(PORT,async()=>{
         console.log(`Server started at ${PORT}`);
-        
+       
     });
 }
 
