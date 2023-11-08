d701347cef705aeb2d45516163f64269c0926e97
Thilak <thilaksaithipparapu6300@gmail.com>
2023-11-08 19:03:52 +0530

    Added get all cities api

--- src/services/city-service.js
+++ src/services/city-service.js
@@ -53,6 +53,16 @@ class CityService{
 
     }
 
+    async getAllCities() {
+        try {
+            const cities = await this.CityRepository.getAllCities();
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw {error};
+        }
+    }
+
 }
 
 module.exports = CityService;
