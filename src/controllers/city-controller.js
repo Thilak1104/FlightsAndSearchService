ae07e44b0090a7bac4e2310aa608c65c75f1264b
Thilak <thilaksaithipparapu6300@gmail.com>
2023-11-08 12:02:35 +0530

    minor fixes

--- src/controllers/city-controller.js
+++ src/controllers/city-controller.js
@@ -8,8 +8,9 @@ const { CityService } = require('../services/index');
 const cityService = new CityService();
 
 const create = async (req,res)=>{
-    console.log("in controller")
+  
     try {
+      
         const city = await cityService.createCity(req.body);
         return res.status(201).json({
             data:city,
@@ -77,7 +78,7 @@ const get = async (req,res)=>{
 
 // Patch -> /city/:id -> req.body
 const update = async (req,res)=>{
-    try {
+    try { 
         const response = await cityService.updateCity(req.params.id,req.body);
         return res.status(200).json({
             data:response,
