b5343da2c31b5161eb5c0796672088f2bb475e70
Thilak <thilaksaithipparapu6300@gmail.com>
2023-11-10 19:21:19 +0530

    Added flights create crud

---
+++ src/services/flight-service.js
@@ -0,0 +1,46 @@
+const {FlightRepository} = require('../repository/index');
+const {AirplaneRepository} = require('../repository/index');
+const { compareTime } = require('../utils/helper');
+class FlightService{
+
+    constructor(){
+        this.airplaneRepository = new AirplaneRepository();
+        this.flightrepository = new FlightRepository();
+        
+    }
+    async createFlight(data){
+        try {
+            if(!compareTime(data.arrivalTime,data.departureTime)){
+                throw{error:'Arrival time cannot be less than departure time'};
+            }
+            const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
+            const flight = await this.flightrepository.createFlight({
+                ...data, totalSeats:airplane.capacity
+            });
+            return flight;
+        } catch (error) {
+            console.log("Something went wrong at service layer");
+            throw {error};
+        }
+    }
+
+    async getFlightData(){
+
+    }
+}
+
+module.exports = FlightService;
+
+
+/**
+ * {
+ *    flightNumber,
+ *    airplaneId,
+ *    departureAirportId,
+ *    arrivalAirportId,
+ *    arrivalTime,
+ *    departureTime,
+ *    price
+ *    totalSeats -> airplane
+ * }
+ */
